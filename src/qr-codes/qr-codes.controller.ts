6282fb05874e86ac1b474dfedfa4dfb09f2932f4
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2025-05-02 17:33:47 +0300

    Decorate qrcode module for swagger

--- src/qr-codes/qr-codes.controller.ts
+++ src/qr-codes/qr-codes.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Param, Header, Res } from '@nestjs/common';
-import { QrCodeService } from './qr-codes.service';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { QrCodeService } from './qr-codes.service';
 
+@ApiTags('QR Codes')
 @Controller('qrcode')
 export class QrCodeController {
     constructor(private readonly qrCodeService: QrCodeService) { }
 
     @Get(':slug')
+    @ApiOperation({ summary: 'Generate a QR code for a venue slug' })
+    @ApiParam({ name: 'slug', description: 'The unique venue slug to encode in the QR code' })
+    @ApiResponse({
+        status: 200,
+        description: 'Returns a PNG image of the QR code',
+        content: { 'image/png': {} }
+    })
     async getQrCode(@Param('slug') slug: string, @Res() res: Response) {
         const base64 = await this.qrCodeService.generateForSlug(slug);
         const imageBuffer = Buffer.from(base64.split(',')[1], 'base64');
