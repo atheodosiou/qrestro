e6eba4b7cbfa3f1cc0f9731a1eff88e7249db57a
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2025-05-17 00:25:08 +0300

    Document with swagger the upload contoller

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,3 @@
-// upload.controller.ts
 import {
   Controller,
   Post,
@@ -11,7 +10,17 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Upload')
+@ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 @Controller('upload')
 export class UploadController {
@@ -19,6 +28,31 @@ export class UploadController {
 
   @Post('image')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload and compress an image' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Image file to upload',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Image uploaded successfully',
+    schema: {
+      example: {
+        message: 'Image uploaded successfully',
+        url: '/uploads/compressed-1715860012123.jpg',
+      },
+    },
+  })
+  @ApiResponse({ status: 400, description: 'Invalid file type' })
   async uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req) {
     if (!file || !file.mimetype.startsWith('image/')) {
       throw new BadRequestException('Invalid file type');
